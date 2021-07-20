4eb61eda93ae660877e2e29abe238adfd7d631ab
Maria Canals <mariacanals23@gmail.com>
2021-07-20 16:27:15 +0200

    v1

--- js/requests.js
+++ js/requests.js
@@ -1,9 +1,4 @@
 const getCountriesFromApi = async () => {
-	// let data = await fetch('https://restcountries.eu/rest/v2/all', {
-	// 	method: 'GET',
-	// });
-	// let response = await data.json();
-	// return response;
 	return axios.get('https://restcountries.eu/rest/v2/all').then(response => {
 		return response.data;
 	});
