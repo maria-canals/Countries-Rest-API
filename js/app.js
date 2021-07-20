943ad2dae589d577e6bf65db8e9f7c6ddbe80677
Maria Canals <mariacanals23@gmail.com>
2021-07-20 18:49:35 +0200

    ok

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 import { getCountriesFromApi } from './requests.js';
 import { clearContainer, numberWithDots } from '../utils/utils.js';
-import { showDetails } from './details.js';
+// import { showDetails } from './details.js';
 
 const container = document.getElementById('container');
 const searchBar = document.getElementById('search');
@@ -125,5 +125,66 @@ toggleModeButton.addEventListener('click', () => {
 	document.body.classList.toggle('dark_mode');
 	localStorage.setItem('theme', currentTheme);
 });
+
+//Details
+
+const modalTitle = document.querySelector('.modal-title');
+const modalBody = document.querySelector('.modal-body');
+
+const countriesFromLocalStorage = localStorage.getItem('countries');
+
+const response = JSON.parse(countriesFromLocalStorage);
+
+const showDetails = event => {
+	let selectedCountry;
+	if (event.target.parentNode.id) {
+		selectedCountry = event.target.parentNode.id;
+	} else {
+		selectedCountry = event.target.parentNode.parentNode.id;
+	}
+
+	modalTitle.textContent = selectedCountry;
+	renderCountry(selectedCountry);
+};
+
+const filterData = (dataType, countrySelected) => {
+	const dataToIterate = dataType;
+	for (const country of response) {
+		if (country.name === countrySelected) {
+			let dataType = Object.entries(country[dataToIterate]);
+			let data = [];
+			for (const el of dataType) {
+				for (const a of el) {
+					dataToIterate === 'currencies'
+						? data.push(a.code)
+						: data.push(a.name);
+				}
+			}
+			const filteredData = data.filter(c => c != undefined);
+			return filteredData;
+		}
+	}
+};
+
+const renderCountry = selectedCountry => {
+	for (const country of response) {
+		if (country.name === selectedCountry) {
+			const currencies = filterData('currencies', selectedCountry);
+			const languages = filterData('languages', selectedCountry);
+			modalBody.innerHTML = `
+			        <img src="${
+								country.flag
+							}" class="modal-card-img-top" style="width: 70%; alt="${selectedCountry}">
+			        <div class="modal-card-body">
+			            <p class="card-text">Native Name: ${country.nativeName}</p>
+			            <p class="card-text">Population:
+						 ${numberWithDots(country.population)}</p>
+			            <p class="card-text">Region: ${country.region} </p>
+						<p class="card-text">Currencies: ${currencies} </p>
+						<p class="card-text">Languages: ${languages} </p>
+			        </div>`;
+		}
+	}
+};
 
 getCountries();
